23012d6909628ddabd18f37ee0cdb0df2591d518
Anwar <anwarhusengit@gmail.com>
2024-10-24 16:33:32 +0530

    changes in js

--- scriptHome.js
+++ scriptHome.js
@@ -6,7 +6,7 @@ modeButton.addEventListener("click", () => {
   if (mode) {
     body.style.backgroundColor = "black";
     body.style.color = "white";
-
+    modeButton.setAttribute("src", "light1.jpg");
     body.querySelector("#enjoy").style.color = "white";
     body.querySelector("ul").style.backgroundColor = "black";
     body.querySelector("ul").style.borderColor = "white";
@@ -14,6 +14,7 @@ modeButton.addEventListener("click", () => {
   } else {
     body.style.backgroundColor = "white";
     body.style.color = "black";
+    modeButton.setAttribute("src", "dark4.png");
     body.querySelector("ul").style.backgroundColor = "white";
     body.querySelector("ul").style.color = "black";
     body.querySelector("#enjoy").style.color = "black";
